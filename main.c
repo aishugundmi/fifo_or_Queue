9cfd86dd629cad40c03560e5bb478952dbc72833
aishugundmi <58282082+aishugundmi@users.noreply.github.com>
2020-12-05 18:48:47 +0530

    Add files via upload

--- main.c
+++ main.c
@@ -9,18 +9,26 @@ fifo_t* fifo_1, *fifo_2;
 int main()
 {
     int val;
+    //creating fifo 1 for uart rx data collection...
     fifo_1 = fifo_create(100);
+
+    //creating another fifo for different purpose, say adc isr to fifo
     fifo_2 = fifo_create(100);
-    fifo_push(fifo_1, 123);
-    fifo_push(fifo_2, 567);
 
-    if(fifo_pop(fifo_1, &val) == 0) {
-        printf("fifo_1 read = %d\n", val);
+    for(int i = 0; i < 10; i++)
+        fifo_push(fifo_1, i);
+
+    for(int i = 0; i < 10; i++)
+        fifo_push(fifo_2, i*10);
+
+
+    while(fifo_pop(fifo_1, &val) == 0) {
+        printf("%d\n", val);
+    }
+    while(fifo_pop(fifo_2, &val) == 0) {
+        printf("%d\n", val);
     }
 
-    if(fifo_pop(fifo_2, &val) == 0) {
-        printf("fifo_2 read = %d\n", val);
-    }
 
     return 0;
 }
